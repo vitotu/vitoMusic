f2189352dcfd1c412a5f3053be1e3d02e9912dc4
vito <vitotu@qq.com>
2022-07-17 21:50:00 +0800

    add main page and static resource

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,29 +1,35 @@
 // index.ts
 // 获取应用实例
+import request from '../../utils/request.ts'
 const app = getApp<IAppOption>()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    bannerList:['/static/images/nvsheng.jpg','/static/images/nvsheng.jpg','/static/images/logo.png'],
+    recommendList:<Array<{picUrl:string, name:string}>>[],
+    topList:<Array<{name:string, tracks:{al:{picUrl:string}}[]}>>[]
   },
   // 事件处理函数
   bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
+
+  },
+  onLoad: async function (options) {
+    let bannerList:Array<{banners:Array<string>}> = await request('/banner', {type:2});
+    this.setData({
+      bannerList:bannerList.banners,
     })
   },
-  onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+  toRecommendSong(){
+    // TODO
+    wx.navigateTo({
+      url: ''
+    })
+  },
+  toOther(){
+    // TODO
+    wx.navigateTo({
+      url:''
+    })
   },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
