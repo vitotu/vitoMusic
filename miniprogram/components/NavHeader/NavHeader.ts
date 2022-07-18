6eaf014cf4e293b1f726a760d0fa9c77d4efea31
vito <vitotu@qq.com>
2022-07-19 00:22:08 +0800

    add NavHeader component

---
+++ miniprogram/components/NavHeader/NavHeader.ts
@@ -0,0 +1,30 @@
+// components/NavHeader.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type: String,
+      value: 'title default value'
+    },
+    nav:{
+      type: String,
+      value: 'nav default value'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
